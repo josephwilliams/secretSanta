7a17d9e338557518abdc83cf44bf9498b568434a
josephwilliams <josephwilli12@gmail.com>
2016-11-13 12:21:23 -0800

    users data flowing through

--- components/splash.js
+++ components/splash.js
@@ -58,6 +58,7 @@ export default class Splash extends Component {
         { !currentUser &&  <WelcomeText /> }
         <Countdown />
         <img src={'./images/christmas_village.png'} style={{ borderRadius: '15px', margin: '5px 10px 20px 0' }}></img>
+        <UserList />
       </div>
     );
   }
--- components/userlist.js
+++ components/userlist.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
+
+var firebase = require('firebase/app');
+require('firebase/auth');
+require('firebase/database');
 
 export default class UserList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+    };
+  }
+
+  componentDidMount() {
+    let that = this;
+    let usersObjectRef = firebase.database().ref('people/');
+    usersObjectRef.on('value', function(snapshot) {
+      let usersObject = snapshot.val();
+      let users = [];
+
+      _.forOwn(usersObject, function(value, key) {
+        users.push(value);
+      });
+
+      that.setState({ users: users });
+    });
+  }
+
   render() {
-    const { users } = this.props;
+    const { users } = this.state;
 
     return (
       <div className={'userlist-wrapper'}>
         {users.map((user, index) => {
           return (
             <div className={'userlist-item'} key={index}>
-              {user}
+              {user.name}
             </div>
           );
         })
