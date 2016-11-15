73753fa21c291ae0c477c63c7afe6ba3b69d7762
josephwilliams <josephwilli12@gmail.com>
2016-11-15 13:52:25 -0800

    added rules to splash welcome text

--- components/welcome-text.js
+++ components/welcome-text.js
@@ -7,6 +7,9 @@ const WelcomeText = () => {
       <div className={'text-wrapper'}>
         <h2>{'welcome to the Williams family secret santa!'}</h2>
         <p>{'sign up or log in to participate.'}</p>
+        <p>{'Rules:'}</p>
+        <p>{'1. One account per person'}</p>
+        <p>{'2. The random draw will occur once everyone has signed up'}</p>
       </div>
     </div>
   );
